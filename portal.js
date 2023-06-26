8bf28a1e1bd308c3c3ca7a3acec833b901d1ba00
Ohm-sudo <45316019+Ohm-sudo@users.noreply.github.com>
2023-06-26 16:18:23 -0400

    Add files via upload

--- portal.js
+++ portal.js
@@ -4,7 +4,7 @@ function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 
-    var button = document.getElementById("darkMode")
+    var button = document.getElementById("darkModeB")
     var img = document.getElementById("york-logo");
 
     if(buttonState === 'default') {
@@ -17,4 +17,32 @@ function darkMode() {
         button.innerHTML = "🌙 Dark Mode";
         img.src = "pictures/york-logo.png";
     }
-  } 
+  }
+
+  function toggleTag(name) {
+    var tag = document.getElementById(name);
+
+    
+
+    if(name == "menu-items")
+    {
+        if(subtitle.style.display === 'none') {
+            subtitle.style.display = 'flex';
+            darkModeB.style.display = 'flex';
+            tag.style.display = 'none';
+        }
+        else {
+            subtitle.style.display = 'none';
+            darkModeB.style.display = 'none';
+            tag.style.display = 'flex';
+        }
+    }
+    else
+    {
+        if(tag.style.display === 'none') {
+            tag.style.display = 'inline-block';
+        } else {
+            tag.style.display = 'none';
+        }
+    }
+  }
