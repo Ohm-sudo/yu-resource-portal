8b6db4be9e69a9cb1ea186a5ad830243d6e5afba
Ohm-sudo <45316019+Ohm-sudo@users.noreply.github.com>
2023-06-27 13:01:43 -0400

    Add files via upload

--- portal.js
+++ portal.js
@@ -1,28 +1,21 @@
-var buttonState = 'default';
-
 function darkMode() {
     var element = document.body;
-    element.classList.toggle("dark-mode");
+    var darkModeToggle = document.getElementById("darkModeB")
+    var isDarkMode = element.classList.toggle("dark-mode");
 
-    var button = document.getElementById("darkModeB")
-    var img = document.getElementById("york-logo");
+    // Store dark mode preferences in localStorage
+    localStorage.setItem('darkMode', isDarkMode)
 
-    if(buttonState === 'default') {
-        buttonState = "alternate";
-        button.innerHTML = "☀️ Light Mode";
-        img.src = "pictures/york-logo-dm.png";
-    }
-    else {
-        buttonState = "default";
-        button.innerHTML = "🌙 Dark Mode";
-        img.src = "pictures/york-logo.png";
-    }
-  }
-
-  function toggleTag(name) {
-    var tag = document.getElementById(name);
-
+    // Update toggle button based on current mode
+    darkModeToggle.textContent = isDarkMode ? '☀️ Light Mode' : '🌙 Dark Mode';
     
+    var img = document.getElementById("york-logo");
+    var imageSrc = isDarkMode ? "pictures/york-logo-dm.png" : "pictures/york-logo.png";
+    img.src = imageSrc;
+}
+
+function toggleTag(name) {
+    var tag = document.getElementById(name);
 
     if(name == "menu-items")
     {
@@ -37,12 +30,57 @@ function darkMode() {
             tag.style.display = 'flex';
         }
     }
-    else
-    {
-        if(tag.style.display === 'none') {
-            tag.style.display = 'inline-block';
-        } else {
-            tag.style.display = 'none';
-        }
+}
+
+function checkCheckboxState(checkbox) {
+    var checkboxId = checkbox.id;
+    var targetId = checkbox.dataset.target;
+    var targetElement = document.getElementById(targetId);
+
+    var checkboxLabels = [];
+
+    for (var i = 1; i <= 9; i++) {
+        var label = "Checkbox" + i;
+        checkboxLabels.push({ id: "checkbox" + i, label: label});
     }
-  }
+
+    var label = checkboxLabels.find(function(item) {
+        return item.id === checkboxId;
+    });
+
+    if(checkbox.checked) {
+        targetElement.style.display = 'inline-block';
+    } else {
+        targetElement.style.display = 'none';
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Check dark mode preference on load
+    var darkModeToggle = document.getElementById('darkModeB');
+    
+    // Retrieve dark mode preference from localStorage
+    var isDarkMode = localStorage.getItem('darkMode');
+
+    if(isDarkMode === 'true') {
+        document.body.classList.add('dark-mode');
+        darkModeToggle.textContent = '☀️ Light Mode';
+    }
+    else {
+        document.body.classList.remove('dark-mode');
+        darkModeToggle.textContent = '🌙 Dark Mode';
+    }
+
+    var img = document.getElementById("york-logo");
+    var imageSrc = isDarkMode === 'true' ? "pictures/york-logo-dm.png" : "pictures/york-logo.png";
+    img.src = imageSrc;
+    
+    // Check state of each checkbox
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach(function(checkbox) {
+        checkbox.addEventListener("change", function(event) {
+            checkCheckboxState(event.target);
+        });
+        checkCheckboxState(checkbox);
+    });
+});
