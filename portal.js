a201db56f25077bb71e8889cae4e33ca265f5f29
Ohm-sudo <45316019+Ohm-sudo@users.noreply.github.com>
2023-07-13 15:36:51 -0400

    Refined menu bar, icons, content layout

--- portal.js
+++ portal.js
@@ -144,3 +144,21 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+window.addEventListener('scroll', function() {
+    var menu = document.getElementById('menu-bar');
+    var darkModeIcon = document.getElementById('darkModeB');
+    var menuItems = document.getElementById('menu-items');
+    var scrollPosition = window.scrollY;
+
+    if(scrollPosition > 0) {
+        menu.classList.add('fixed');
+        darkModeIcon.classList.add('fixed');
+        menuItems.classList.add('fixed');
+
+    } else {
+        menu.classList.remove('fixed');
+        darkModeIcon.classList.remove('fixed');
+        menuItems.classList.remove('fixed');
+    }
+});
